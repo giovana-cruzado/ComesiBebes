0a1651b212f1e3247b75ce7eb98d20d3bdffefd5
felipe-botura <felipebotura7@gmail.com>
2025-09-15 10:28:10 -0300

    problemas

--- ComesiBebes/components/BottomSheet.tsx
+++ ComesiBebes/components/BottomSheet.tsx
@@ -1,13 +1,24 @@
-import React, { forwardRef, useMemo } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet'
+import React, { forwardRef, useCallback, useMemo } from 'react'
+import { StyleSheet, Text, touchableOpacity View } from 'react-native'
+import { BottomSheetModal, BottomSheetView, bottomSheetbackdrop, useBottomSheetModal } from '@gorhom/bottom-sheet'
+import Colors from '@/constants/Colors';
+import Ionicons from '@expo/vector-icons/ionicons';
+import { Link  } from 'expo-router';
 
 export type Ref = BottomSheetModal;
 
 const BottomSheet = forwardRef<Ref>((props, ref) => {
   const snapPoints = useMemo(() => ['50%'], [])
+  const renderBackdrop = useCallback((props: any) =>
+  <bottomSheetbackdrop apperarsOnIndex={0} disapearsOnIndex={-1} {...props}/>, [])
+  const { dismiss } = useBottomSheetModal();
   return (
-    <BottomSheetModal ref={ref} snapPoints={snapPoints}>
+    <BottomSheetModal 
+    ref={ref} 
+    snapPoints={snapPoints}
+    backdropComponent={renderBackdrop}
+    overDragresistanceFactor={0}
+    >
       <BottomSheetView style={styles.contentContainer}>
           <Text>BottomSheet</Text>
       </BottomSheetView>
